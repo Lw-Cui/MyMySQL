b6754023a59ffce9e663c6b7aed9d004bf560d38
lw <cui@hellolw.com>
2016-06-03 18:54:08 +0800

    add heeader file

--- include/header.hpp
+++ include/header.hpp
@@ -1,5 +1,11 @@
 #pragma once
 #include <iostream>
+#include <regex>
+#include <map>
+#include <vector>
 #include <string>
 #include <mysql++.h>
-int QuerySQL(mysqlpp::Connection &conn, const std::string& Query_str);
+int QuerySQL(mysqlpp::Connection &conn, 
+	const std::string& Query_str);
+std::vector<std::string> 
+	Regex(const std::string &origin, const std::string &pattern);
