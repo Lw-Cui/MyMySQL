7200304458f4331db9f45706c2373fe224f4fedf
lw <cui@hellolw.com>
2016-06-03 20:57:44 +0800

    upgrade header

--- include/header.hpp
+++ include/header.hpp
@@ -5,7 +5,9 @@
 #include <vector>
 #include <string>
 #include <mysql++.h>
-int QuerySQL(mysqlpp::Connection &conn, 
-	const std::string& Query_str);
-std::vector<std::string> 
-	Regex(const std::string &origin, const std::string &pattern);
+std::vector<std::string> QuerySQL(mysqlpp::Connection &conn, const std::string &Query_str);
+std::vector<std::string> Argument(const std::string &origin);
+std::vector<std::string> Dbname(const std::string &origin);
+std::vector<std::string> Condition(const std::string &origin);
+const std::string Build(const std::vector<std::string> &arg,
+	 const std::vector<std::string> &db, const std::vector<std::string> &con);
