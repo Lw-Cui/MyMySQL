b19a520335ee478631f08775a2185bed0c4058f8
lw <cui@hellolw.com>
2016-06-12 21:32:26 +0800

    implement basic parser

---
+++ include/parser.hpp
@@ -0,0 +1,110 @@
+#pragma once
+#include <iostream>
+
+class Lexer {
+public:
+	enum Token {
+		TokEOF = -1,
+		TokName = -2,
+		TokNumber = -3,
+		TokIdentifier = -4,
+		TokArgs = -5,
+		TokCond = -6,
+		TokDb = -7,
+	};
+
+	Lexer(const std::string &exp):expression{exp}, currentTok{TokEOF} {GetNextTok();}
+	inline double GetNumVal() {return numVal;}
+	inline std::string GetNameStr() {return nameStr;}
+	inline Token GetTok() {return currentTok;}
+	Token GetNextTok() {
+		int beg{expression.get()}; 
+		if (beg == EOF) {
+			currentTok = TokEOF;
+		} else if (isalpha(beg)) {
+			expression.putback(beg); nameStr.clear(); 
+			while ((beg = expression.get()) && isalnum(beg))
+				nameStr.push_back(beg);
+			expression.putback(beg); currentTok = TokName;
+		} else if (isdigit(beg)) {
+			expression.putback(beg);
+			expression >> numVal; currentTok = TokNumber;
+		} else if (isspace(beg)) {
+			return GetNextTok();
+		} else {
+			expression.putback(beg); nameStr.clear(); 
+			while ((beg = expression.get()) && !isalnum(beg) && !isspace(beg) && beg != EOF)
+				nameStr.push_back(beg);
+			expression.putback(beg); 
+			if (nameStr == "(") currentTok = TokArgs;
+			else if (nameStr == "@") currentTok = TokDb;
+			else if (nameStr == "]") currentTok = TokCond;
+			else currentTok = TokIdentifier;
+		}
+		return currentTok;
+	}
+
+private:
+	std::istringstream expression;
+	std::string nameStr;
+	double numVal;
+	Token currentTok;
+};
+
+class ExprAST {
+public:
+	virtual ~ExprAST(){}
+	virtual void print(std::ostream &out) = 0;
+};
+
+class NumberExprAST: public ExprAST {
+public:
+	NumberExprAST(double v):Val{v} {}
+	virtual void print(std::ostream &out) override {out << Val << " ";}
+private:
+	double Val;
+};
+
+class NameExprAST: public ExprAST {
+public:
+	NameExprAST(const std::string &n):Vname{n}{}
+	virtual void print(std::ostream &out) override {out << Vname << " ";}
+private:
+	std::string Vname;
+};
+
+class BinaryExprAST: public ExprAST {
+public:
+	BinaryExprAST(const std::string &op,
+		std::shared_ptr<ExprAST> left, std::shared_ptr<ExprAST> right)
+	:Op{op}, LHS{std::move(left)}, RHS{std::move(right)} {}
+	virtual void print(std::ostream &out) override {
+		LHS->print(out); out << " " << Op; RHS->print(out);}
+private:
+	std::string Op;
+	std::shared_ptr<ExprAST> LHS, RHS;
+};
+
+class ExpressionAST: public ExprAST {
+public:
+	ExpressionAST(std::vector<std::shared_ptr<ExprAST>> v):ptr{std::move(v)}{}
+	virtual void print(std::ostream &out) override {
+		for (auto &p: ptr) p->print(out);}
+private:
+	std::vector<std::shared_ptr<ExprAST>> ptr;
+};
+
+/*
+class QueryExprAST: public ExprAST {
+public:
+	QueryExprAST(
+		std::shared_ptr<ColumnExprAST> col,
+		std::shared_ptr<DataBaseExprAST>d, 
+		std::shared_ptr<ConditionExprAST> con)
+	:Col{col}, Db{d}, Con{con}{}
+private:
+	std::shared_ptr<ColumnExprAST> Col;
+	std::shared_ptr<DataBaseExprAST> Db;
+	std::shared_ptr<ConditionExprAST> Con;
+};
+*/
--- parser/parser.cpp
+++ parser/parser.cpp
@@ -1,56 +1,129 @@
 #include <string>
+#include <vector>
+#include <memory>
 #include <iostream>
 #include <sstream>
+#include <parser.hpp>
 using namespace std;
 namespace {
 	const char *MYQUERY = "(productNo, productName) @Product @OrderDetail [quantity > 3]";
 }
 
-class Lexer {
-public:
-	enum Token {
-		TokEOF = -1,
-		TokName = -2,
-		TokNumber = -3,
-		TokIdentifier = -4,
-	};
+inline std::shared_ptr<ExprAST> LogError(const std::string& error) {
+	std::cerr << error << std::endl;
+	return nullptr;
+}
 
-	Lexer(const std::string &exp):expression{exp} {}
-	inline double GetNumVal() {return numVal;}
-	inline std::string GetNameStr() {return nameStr;}
-	Token GetTok() {
-		int beg{expression.get()}; 
-		if (beg == EOF) {
-			return TokEOF;
-		} else if (isalpha(beg)) {
-			expression.putback(beg); nameStr.clear(); 
-			while ((beg = expression.get()) && isalnum(beg))
-				nameStr.push_back(beg);
-			expression.putback(beg); return TokName;
-		} else if (isdigit(beg)) {
-			expression.putback(beg);
-			expression >> numVal; return TokNumber;
-		} else if (isspace(beg)) {
-			return GetTok();
-		} else {
-			expression.putback(beg); nameStr.clear(); 
-			while ((beg = expression.get()) && !isalnum(beg) && !isspace(beg) && beg != EOF)
-				nameStr.push_back(beg);
-			expression.putback(beg); return TokIdentifier;
-		}
+inline std::shared_ptr<ExprAST> ParseNameExpr(Lexer &lex) {
+	auto v = std::make_shared<NameExprAST>(lex.GetNameStr());
+	lex.GetNextTok();
+	return std::move(v);
+}
+
+inline std::shared_ptr<ExprAST> ParseNumberExpr(Lexer &lex) {
+	auto v = std::make_shared<NumberExprAST>(lex.GetNumVal());
+	lex.GetNextTok();
+	return std::move(v);
+}
+
+inline std::shared_ptr<ExprAST> ParseIdentifierExpr(Lexer &lex) {
+	auto v = std::make_shared<NameExprAST>(lex.GetNameStr());
+	lex.GetNextTok();
+	return std::move(v);
+}
+
+inline std::shared_ptr<ExprAST> ParseDatabaseExpr(Lexer &lex) {
+	std::vector<std::shared_ptr<ExprAST>> v;
+	while (lex.GetTok() == Lexer::Token::TokDb) {
+		lex.GetNextTok(); v.push_back(ParseNameExpr(lex));
 	}
+	return std::make_shared<ExpressionAST>(v);
+}
 
-private:
-	std::istringstream expression;
-	std::string nameStr;
-	double numVal;
-};
+inline std::shared_ptr<ExprAST> ParseColumnExpr(Lexer &lex) {
+	std::vector<std::shared_ptr<ExprAST>> v;
+	lex.GetNextTok(); 
+	while ((lex.GetTok() == Lexer::Token::TokIdentifier && lex.GetNameStr() != ")")
+		|| lex.GetTok() == Lexer::Token::TokName)
+		if (lex.GetTok() == Lexer::Token::TokName)
+			v.push_back(ParseNameExpr(lex));
+		else
+			lex.GetNextTok();
+	lex.GetNextTok(); return std::make_shared<ExpressionAST>(v);
+}
+
+inline std::shared_ptr<ExprAST> ParseSimpleCondExpr(Lexer &lex) {
+	std::vector<std::shared_ptr<ExprAST>> v;
+	lex.GetNextTok(); 
+	while (lex.GetNameStr() != "]")
+		switch (lex.GetTok()) {
+		case Lexer::Token::TokName:
+			v.push_back(ParseNameExpr(lex)); break;
+		case Lexer::Token::TokNumber:
+			v.push_back(ParseNumberExpr(lex)); break;
+		case Lexer::Token::TokIdentifier:
+			v.push_back(ParseIdentifierExpr(lex)); break;
+		default: break;
+		}
+	lex.GetNextTok(); return std::make_shared<ExpressionAST>(v);
+}
+
+inline std::shared_ptr<ExprAST> ParseBinOpRHS
+	(int ExprPrec, std::shared_ptr<ExprAST> LHS) {
+	/*
+	ParseSingleCondExpr();
+	*/
+}
+
+
+inline std::shared_ptr<ExprAST> ParseConditionExpr(Lexer &lex) {
+	//auto ptr = ParseSQL
+	/*	
+	LHS = ParseSingleCondExpr();
+	return ParseBinOpRHS(0, LHS);
+	*/
+}
+
+inline std::shared_ptr<ExprAST> ParseSQL(Lexer &lex) {
+	switch (lex.GetTok()) {
+	case Lexer::Token::TokDb:
+		return ParseDatabaseExpr(lex);
+	case Lexer::Token::TokCond:
+		return ParseColumnExpr(lex);
+	case Lexer::Token::TokArgs:
+		return ParseConditionExpr(lex);
+	default:
+		return LogError("unknown token when expecting an expression");
+	}
+}
+
+inline std::shared_ptr<ExprAST> ParseSingleCondExpr(Lexer &lex) {
+	switch (lex.GetTok()) {
+	case Lexer::Token::TokNumber:
+	case Lexer::Token::TokIdentifier:
+	case Lexer::Token::TokName:
+		return ParseSimpleCondExpr(lex);
+	default:
+		return ParseSQL(lex);
+	}
+}
+
 
 int main() {
 	Lexer lex{MYQUERY};
+	/*
 	Lexer::Token tok;
-	while ((tok = lex.GetTok()) != Lexer::Token::TokEOF)
+	cout << "Lexer:" << endl;
+	while ((tok = lex.GetNextTok()) != Lexer::Token::TokEOF)
 		if (tok == Lexer::Token::TokNumber) cout << lex.GetNumVal() << endl;
 		else cout << lex.GetNameStr() << endl;
+	*/
+	cout << "Parser:" << endl;
+	auto ptr = ParseColumnExpr(lex);
+	ptr->print(cout);
+	ptr = ParseDatabaseExpr(lex);
+	ptr->print(cout);
+	ptr = ParseSimpleCondExpr(lex);
+	ptr->print(cout);
 	return 0;
 }
